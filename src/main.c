631791e53ee1e32e4daef359f76a1904d44e9c11
Tony Houghton <h@realh.co.uk>
2016-11-26 19:41:29 +0000

    Open input/output files.

--- src/main.c
+++ src/main.c
@@ -17,16 +17,81 @@
     Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
+#include <errno.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#ifdef _WIN32
+#include <io.h>
+#include <fcntl.h>
+#endif
 
 #include "options.h"
+
+#ifdef _WIN32
+void set_stdin_to_binary(void)
+{
+    _setmode(0, O_to_binary);
+}
+
+void set_stdout_to_binary(void)
+{
+    _setmode(1, O_to_binary);
+}
+#else
+void set_stdin_to_binary(void)
+{
+    freopen(NULL, "rb", stdin);
+}
+
+void set_stdout_to_binary(void)
+{
+    freopen(NULL, "wb", stdout);
+}
+#endif
 
 int main(int argc, char **argv)
 {
     NormalmapOptions *options = normalmap_options_get(argc, argv);
+    FILE *fp_in, *fp_out;
 
+    /*
     printf("xyz = '%s', unsigned = %d, normalise = %d\n",
             options->xyz, options->unsigned_z, options->normalise);
+    */
+
+    if (!options->input || !strcmp(options->input, "-"))
+    {
+        set_stdin_to_binary();
+        fp_in = stdin;
+    }
+    else
+    {
+        fp_in = fopen(options->input, "rb");
+        if (!fp_in)
+        {
+            fprintf(stderr, "Could not open input file '%s': %s\n",
+                    options->input, strerror(errno));
+            exit(1);
+        }
+    }
+
+    if (!options->input || !strcmp(options->output, "-"))
+    {
+        set_stdout_to_binary();
+        fp_out = stdout;
+    }
+    else
+    {
+        fp_out = fopen(options->output, "wb");
+        if (!fp_out)
+        {
+            fprintf(stderr, "Could not open output file '%s': %s\n",
+                    options->output, strerror(errno));
+            exit(1);
+        }
+    }
 
     return 0;
 }
